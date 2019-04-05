5080170e14131ba1aca086b3614692da68ee4962
xyweshing <nikitasidorenko10@inbox.ru>
2019-04-05 15:45:12 +0300

    Added entry point

---
+++ src/AIT4/Sidorenko/wdad/Apllication.java
@@ -0,0 +1,11 @@
+package AIT4.Sidorenko.wdad;
+
+public class Apllication {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am Nikita Sidorenko, and I am not a monkey");
+
+    }
+}
+
